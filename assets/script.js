6d30f371a9b9bb6201c78bf77dc1c6a8a43c7dcf
jessny <jessnyjoseph@gmail.com>
2021-01-15 20:28:46 -0800

    screenshot of second assignment added

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,21 @@ function displayProject() {
             <div class="large-article-header-content">    
             </div>
         </div>
-        <h1>What's for Dinner?</h1>
-        <a class="button button-rounded-hover">Deployed Site</a>
+        <h1 id="titleProjectOne">What's for Dinner?</h1>
+        <p id="project1About">This application allows a user to decide what they would like to eat for dinner. 
+        With a random recipe generator, a recipe search bar, or a form to search
+        for the top ten rated restaurants in their city, they can make a dinner decision 
+        quicker than ever. </p>
+        <a class="button button-rounded-hover" target="_blank" href="https://vubao2303.github.io/Project-1/">Deployed Site</a>
+        <a class="githubP1" target="_blank" href="https://github.com/vubao2303/Project-1">Github Repository</a>
     </div>
 </div>
+<br>
+<div>
+<img src="" alt="Girl in a jacket" style="width:500px;height:600px;">
+</div>
+
+
 `)}
 
 
